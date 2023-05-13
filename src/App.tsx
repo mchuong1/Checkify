092c0823d41b38bbda7623f1d33b7777d45bd983
Matthew Chuong <matthew.chuong@passportinc.com>
2023-05-13 14:03:04 -0400

    test on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import styles from './App.module.scss';
 import { TaskCard } from './components/task-card/task-card';
+import { Button } from './components/button/button';
+import { useState } from 'react';
+import _ from 'lodash';
+import { NavigationBar } from './components/navigation-bar/navigation-bar';
 
 function App() {
+    const [taskCards, setTaskCards] = useState<number[]>([]);
+
+    const handleCardClick = (indexToRemove: number) => {
+        setTaskCards(_.filter(taskCards, (cards, index) => index !== indexToRemove));
+    };
+
     return (
         <div className={styles.App}>
-            <TaskCard title={"The very first task"}/>
+            <NavigationBar title="Checkify" navItems={[]} />
+            <div className={styles.cardListContainer}>
+                {_.map(taskCards, (card, index) => (
+                    <TaskCard title={`${card} Task`} onClick={() => handleCardClick(index)} />
+                ))}
+            </div>
+            <Button
+                label="Add Task"
+                onClick={() => setTaskCards(_.concat(taskCards, taskCards.length as number))}
+            />
         </div>
     );
 }
